7820a5f3abd27e96f867fe1f63352ee40861ed75
yeongjin-Jung <yeongjin13@gmail.com>
2021-01-27 14:54:44 +0900

    javascript home contact

--- main.js
+++ main.js
@@ -10,3 +10,28 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+
+  scrollIntoView(link);
+});
+
+// Contact button click
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
